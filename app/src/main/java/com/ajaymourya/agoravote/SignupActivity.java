13f61c9f6344952ca6b54c58f4c3f5816da4954c
Ajay Mourya <ajaymourya8553@gmail.com>
2019-03-26 22:06:09 +0530

    Implemented sign up design

--- app/src/main/java/com/ajaymourya/agoravote/SignupActivity.java
+++ app/src/main/java/com/ajaymourya/agoravote/SignupActivity.java
@@ -6,22 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-
-    public void login(View view) {
-        Intent intent = new Intent(this, SignupActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_signup);
     }
 
     public void signup(View view) {
-        Intent intent = new Intent(this, SignupActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
