0b9e6f164e6d921500dea3cbc4547617c68b81d4
Ajay Mourya <ajaymourya8553@gmail.com>
2019-03-27 21:10:11 +0530

    implemented navigation drawer design

--- app/src/main/java/com/ajaymourya/agoravote/DashboardActivity.java
+++ app/src/main/java/com/ajaymourya/agoravote/DashboardActivity.java
@@ -1,16 +1,64 @@
 package com.ajaymourya.agoravote;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_main);
+
+        // Set a Toolbar to replace the ActionBar.
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.menu_logout:
+                intent();
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void intent(){
+        Intent intent  = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
