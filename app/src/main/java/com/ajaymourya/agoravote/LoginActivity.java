91fd6146cb91e30c5bedf6968da513f3019c42fe
Ajay Mourya <ajaymourya8553@gmail.com>
2019-04-09 09:02:49 +0530

    Implemented login and sign up functionality

--- app/src/main/java/com/ajaymourya/agoravote/LoginActivity.java
+++ app/src/main/java/com/ajaymourya/agoravote/LoginActivity.java
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+    private TextInputEditText mUserNameEditText;
+    private TextInputEditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +31,71 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
+        mLoginButton = findViewById(R.id.login_button);
+        mUserNameEditText = findViewById(R.id.username_edittext);
+        mPasswordEditText = findViewById(R.id.password_edittext);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mUserNameEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "enter your username", Toast.LENGTH_SHORT).show();
+                else if (mPasswordEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "enter your password", Toast.LENGTH_LONG).show();
+                else
+                    performLoginOperation();
+            }
+        });
+
+
     }
 
+    private void performLoginOperation() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("password", mPasswordEditText.getText().toString());
+            jsonObject.put("identifier", mUserNameEditText.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-    public void login(View view) {
-        Intent intent = new Intent(this, DashboardActivity.class);
-        startActivity(intent);
+        AndroidNetworking.post("https://agora-rest-api.herokuapp.com/api/v1/auth/login")
+                .addJSONObjectBody(jsonObject)// posting json
+                .addHeaders("Content-Type", "application/json")
+                .addHeaders("accept", "application/json")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("response", "" + response);
+                        try {
+                            JSONObject token = response.getJSONObject("token");
+                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        //loadToast.error();
+                        Toast.makeText(getApplicationContext(), "wrong username or password", Toast.LENGTH_SHORT).show();
+                        // handle error
+                        Log.d("errorb", "" + error.getErrorBody());
+                        Log.d("errorr", "" + error.getResponse());
+                        Log.d("errord", "" + error.getErrorDetail());
+                        Log.d("errorc", "" + error.getErrorCode());
+                        Log.d("errorm", "" + error.getMessage());
+                    }
+                });
     }
 
     public void signup(View view) {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/ajaymourya/agoravote/SignupActivity.java
+++ app/src/main/java/com/ajaymourya/agoravote/SignupActivity.java
@@ -4,10 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.StringRequestListener;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private TextInputEditText mUserNameEditText;
+    private TextInputEditText mFirstNameEditText;
+    private TextInputEditText mLastNameEditText;
+    private TextInputEditText mEmailEditText;
+    private TextInputEditText mPasswordEditText;
+    private Button mSignUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +35,75 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
+        mUserNameEditText = findViewById(R.id.signup_username_edittext);
+        mFirstNameEditText = findViewById(R.id.firstname_edittext);
+        mLastNameEditText = findViewById(R.id.lastname_edittext);
+        mEmailEditText = findViewById(R.id.email_edittext);
+        mPasswordEditText = findViewById(R.id.signup_password_edittext);
+        mSignUpButton = findViewById(R.id.signup_button);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mUserNameEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "please enter username", Toast.LENGTH_SHORT).show();
+                else if (mFirstNameEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "please enter username", Toast.LENGTH_SHORT).show();
+                else if (mLastNameEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "please enter username", Toast.LENGTH_SHORT).show();
+                else if (mEmailEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "please enter username", Toast.LENGTH_SHORT).show();
+                else if (mPasswordEditText.getText().toString().isEmpty())
+                    Toast.makeText(getApplicationContext(), "please enter username", Toast.LENGTH_SHORT).show();
+                else
+                    performSignUpOperation();
+            }
+        });
+
+
     }
 
-    public void signup(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+    private void performSignUpOperation() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("firstName", mFirstNameEditText.getText().toString());
+            jsonObject.put("lastName", mLastNameEditText.getText().toString());
+            jsonObject.put("email", mEmailEditText.getText().toString());
+            jsonObject.put("password", mPasswordEditText.getText().toString());
+            jsonObject.put("identifier", mUserNameEditText.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        AndroidNetworking.post("https://agora-rest-api.herokuapp.com/api/v1/auth/signup")
+                .addJSONObjectBody(jsonObject)// posting json
+                .addHeaders("Content-Type", "application/json")
+                .addHeaders("accept", "application/json")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsString(new StringRequestListener() {
+                    @Override
+                    public void onResponse(String response) {
+                        // do anything with response
+                        Log.d("response", "" + response);
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        // handle error
+                        Log.d("errorb", "" + error.getErrorBody());
+                        Log.d("errorr", "" + error.getResponse());
+                        Log.d("errord", "" + error.getErrorDetail());
+                        Log.d("errorc", "" + error.getErrorCode());
+                        Log.d("errorm", "" + error.getMessage());
+                        Toast.makeText(getApplicationContext(), "" + error.getErrorBody(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
+
 
     public void login(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
